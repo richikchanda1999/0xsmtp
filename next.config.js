fb77a677ac8943dbf83e0b336a5150c150c160c9
Richik Chanda <richikchanda1999@gmail.com>
2022-06-26 16:38:46 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	trailingSlash: true,
+  webpack: function(config, options) {
+		if(!options.isServer) {
+			config.resolve.fallback.fs = false
+		}
+
+		config.experiments = { asyncWebAssembly: true }
+		return config
+	},
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,84 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, {
+	createContext,
+	ReactElement,
+	ReactNode,
+	useMemo,
+} from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { NextPage } from 'next'
+import type { AppContext, AppProps } from 'next/app'
+import App from 'next/app'
+import {
+	allChains,
+	configureChains,
+	createClient,
+	WagmiConfig,
+} from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { publicProvider } from 'wagmi/providers/public'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID
+
+const { chains, provider } = configureChains(allChains, [
+	publicProvider(),
+])
+
+// Set up client
+const client = createClient({
+	autoConnect: true,
+	connectors: [
+		new InjectedConnector({
+			chains,
+			options: {
+				name: 'Injected',
+				shimDisconnect: true,
+			},
+		}),
+		new MetaMaskConnector({
+			chains,
+			options: {
+				shimDisconnect: true,
+			},
+		}),
+		new WalletConnectConnector({
+			chains,
+			options: {
+				infuraId,
+				qrcode: true,
+			},
+		}),
+	],
+	provider,
+})
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+	return (
+		<>
+			<WagmiConfig client={client}>
+					<ChakraProvider>
+						<Component {...pageProps} />
+					</ChakraProvider>
+			</WagmiConfig>
+		</>
+	)
 }
 
+// MyApp.getInitialProps = async(appContext: AppContext) => {
+// 	// calls page's `getInitialProps` and fills `appProps.pageProps`
+// 	const appProps = await App.getInitialProps(appContext)
+// 	return { ...appProps }
+// }
+
+// export default dynamic(() => Promise.resolve(MyApp), {
+//   ssr: false,
+// });
 export default MyApp
